1dc8675e6a8b1915b5a3f91f7cccb295e2871c0f
Eric Razor <eric.razorjr@gmail.com>
2018-02-09 04:28:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+
+window.kittens = ["Milo","otis","Garfield"];
+
+
+function destructivelyAppendKitten(epiphany){
+  kittens.push(epiphany);
+  window.kittens;
+}
+
+function destructivelyPrependKitten(epiphanyAgain){
+  kittens.unshift(epiphanyAgain);
+  window.kittens;
+}
+
+function destructivelyRemoveLastKitten(kitty){
+  kittens.pop(kitty);
+  window.kittens;
+}
+
+function destructivelyRemoveFirstKitten(firstKitteh){
+  kittens.shift(firstKitteh);
+  window.kittens;
+}
+
+function appendKitten(kitty){
+  var newArray = kittens.unshift(kitty)
+  return newArray
+  
+}
